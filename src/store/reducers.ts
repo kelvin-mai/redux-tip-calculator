241c54b49a1bd857cabec0aa63b39a8675c19882
Kelvin Mai <kelvin.mai002@gmail.com>
2020-01-11 18:58:04 -0800

    end of video 1

---
+++ src/store/reducers.ts
@@ -0,0 +1,13 @@
+import { Reducer, Action } from 'redux';
+import { StoreState } from '.';
+
+const initalState: StoreState = {
+  bill: 0,
+  percentage: 0,
+  split: 1,
+};
+
+export const rootReducer: Reducer<StoreState, Action> = (
+  state = initalState,
+  action,
+) => state;
